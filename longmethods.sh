9e8c4805701914492cd5a23bffad6b5a87bec36b
Artjom <artjom.lell@web.de>
2019-10-19 21:28:08 +0200

    issue4/integrated options for method length and result limit

--- longmethods.sh
+++ longmethods.sh
@@ -1,13 +1,34 @@
 #!/bin/bash
 
-SEARCH_PATH=$1
 TMP_NOT_SORTED="/tmp/temp"
 TMP_SORTED="/tmp/temp_sorted"
-RESULT_LIMIT=$2
+# Default behavior for limit of results and method lengths
+IS_LONG_FROM=38
+RESULT_LIMIT=50
 
-# make sure two parameters are given
-if [[ $# -ne 2 ]]; then
-  echo "Please enter the direcory path and the number of max results"
+# options stuff
+for arg in "$@"; do
+  shift
+  case "$arg" in
+  "--directory") set -- "$@" "-d" ;;
+  "--isLongFrom") set -- "$@" "-f" ;;
+  "--limit") set -- "$@" "-l" ;;
+  *) set -- "$@" "$arg" ;;
+  esac
+done
+
+# Parse short options
+while getopts "f:l:d:" opt; do
+  case "$opt" in
+  "d") SEARCH_PATH=$OPTARG ;;
+  "f") IS_LONG_FROM=$OPTARG ;;
+  "l") RESULT_LIMIT=$OPTARG ;;
+  ?) echo "unsupported options. Please use -d (--directory), -l (--limit) or -f (--isLongFrom) " ;;
+  esac
+done
+
+if [ -z "$SEARCH_PATH" ]; then
+  echo "please run the script with -d option for scanning project path"
   exit 1
 fi
 
@@ -17,19 +38,16 @@ if ! [[ -d $SEARCH_PATH ]]; then
   exit 1
 fi
 
-# make sure second parameter is a number
-if ! [[ $RESULT_LIMIT =~ ^[0-9]+$ ]]; then
-  echo "given second parameter is not a number"
-  exit 1
-fi
-
 # shellcheck disable=SC2162
-read -p "include test packages? (y/n)?" include_test
+read -p "include test packages? (y/n)? " include_test
 
 case $include_test in
 y | Y) echo "test packages are included" ;;
 n | N) echo "test packages are not included" ;;
-*) echo "invalid" ;;
+*)
+  echo "invalid answer (y/n)"
+  exit 1
+  ;;
 esac
 
 function createOrTruncateFile() {
@@ -56,11 +74,10 @@ else
 fi
 
 # next pipeline: sort temp-file/methods by length (descending)
-awk '{ print length($0) " " $0; }' $TMP_NOT_SORTED | sort -r -n | uniq | cut -d " " -f2- >$TMP_SORTED
+awk '{ print length($0) " " $0; }' $TMP_NOT_SORTED | sort -r -n | uniq | cut -d " " -f2- | head --lines="$RESULT_LIMIT" >$TMP_SORTED
 
 RED='\033[0;31m\e[1m'
 YELLOW='\033[0;33m\e[1m'
-CYAN='\033[0;36m\e[1m'
 DEFAULT='\e[0m'
 
 function printLineAndSleep() {
@@ -72,12 +89,12 @@ function printLineAndSleep() {
 COUNTER=0
 while IFS= read -r line; do
   length=$(echo "{$line}" | wc -c)
-  if ((length > 40)); then
+  if ((length > IS_LONG_FROM)); then
     text=${RED}${line}${DEFAULT}
-  elif ((length > 35)); then
+  elif ((length > ((IS_LONG_FROM - 5)))); then
     text=${YELLOW}${line}${DEFAULT}
   else
-    text=${CYAN}${line}${DEFAULT}
+    break
   fi
   printLineAndSleep "${text}""()" 0.08
   COUNTER=$((COUNTER + 1))
